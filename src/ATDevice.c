7d8c886f6dc0291357a749b2637b75c59cbc4e37
Matthew Cifelli <matthewcifelli@gmail.com>
2015-03-24 22:39:11 -0400

    fixed register get and dialing bugs

--- src/ATDevice.c
+++ src/ATDevice.c
@@ -21,7 +21,7 @@ int ATDevice_write(struct ATDevice * self, const char * buffer, size_t start, si
 	// TODO: Write to correct stream
 	// TODO: Ensure stream locking so that no more than one command may be sent
 	// temporarily printing to console output stream
-	printf("\n%s", buffer);
+	printf("\n%s;", buffer);
 }
 
 int ATDevice_answer(struct ATDevice * self) {
@@ -33,56 +33,48 @@ int ATDevice_repeat(struct ATDevice * self) {
 }
 
 int ATDevice_dial(struct ATDevice * self, const char * number, size_t numberLength, enum AT_COMMAND_DIAL_OPTION option) {
-	unsigned int cursor = 0, size;
+	size_t size = AT_COMMAND_DIAL_SIZE + numberLength + AT_COMMAND_DIAL_OPTIONS_SIZE;
+	char buffer[size];
+	char option_char;
 
-	if (!self || !number) {
+	if (!self || !number || !numberLength) {
 		return 0;
 	}
-
-	size = AT_COMMAND_DIAL_SIZE + numberLength + AT_COMMAND_DIAL_OPTIONS_SIZE;
-	char buffer[size];
-
-	// write command to the buffer
-	strncpy(buffer, AT_COMMAND_DIAL, AT_COMMAND_DIAL_SIZE);
-	cursor += AT_COMMAND_DIAL_SIZE;
-
-	// write phone number to the buffer
-	strncpy(buffer + cursor, number, numberLength);
-	cursor += numberLength;
 
 	// write option to the buffer
 	switch (option) {
 		case AT_COMMAND_DIAL_OPTION_PULSE:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_PULSE_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_PULSE_CHAR;
 			break;
 		case AT_COMMAND_DIAL_OPTION_TOUCH_TONE:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_TOUCH_TONE_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_TOUCH_TONE_CHAR;
 			break;
 		case AT_COMMAND_DIAL_OPTION_AWAIT_TONE:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_AWAIT_TONE_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_AWAIT_TONE_CHAR;
 			break;
 		case AT_COMMAND_DIAL_OPTION_REVERT:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_REVERT_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_REVERT_CHAR;
 			break;
 		case AT_COMMAND_DIAL_OPTION_AWAIT_RINGBACK:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_AWAIT_RINGBACK_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_AWAIT_RINGBACK_CHAR;
 			break;
 		case AT_COMMAND_DIAL_OPTION_PAUSE:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_PAUSE_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_PAUSE_CHAR;
 			break;
 		case AT_COMMAND_DIAL_OPTION_REMAIN_IN_COMMAND:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_REMAIN_IN_COMMAND_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_REMAIN_IN_COMMAND_CHAR;
 			break;
 		case AT_COMMAND_DIAL_OPTION_FLASH:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_FLASH_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_FLASH_CHAR;
 			break;
 		case AT_COMMAND_DIAL_OPTION_REDIAL:
-			strncpy(buffer + cursor, AT_COMMAND_DIAL_OPTION_REDIAL_CHAR, AT_COMMAND_DIAL_OPTIONS_SIZE);
+			option_char = AT_COMMAND_DIAL_OPTION_REDIAL_CHAR;
 			break;
 		default:
 			return 0;
 	}
-	cursor += AT_COMMAND_DIAL_OPTIONS_SIZE;
+
+	snprintf(buffer, size + 1, AT_COMMAND_DIAL, number, option_char);
 
 	return ATDevice_write(self, buffer, 0, size);
 }
@@ -104,54 +96,39 @@ int ATDevice_pickUp(struct ATDevice * self) {
 }
 
 int ATDevice_inquire(struct ATDevice * self, unsigned int id) {
-	size_t cursor = 0;
 	size_t size = AT_COMMAND_INFO_SIZE + AT_COMMAND_INFO_OPTIONS_SIZE;
 	char buffer[size];
 
-	// write command to buffer
-	strncpy(buffer + cursor, AT_COMMAND_INFO, AT_COMMAND_INFO_SIZE);
-	cursor += AT_COMMAND_INFO_SIZE;
+	if (AT_COMMAND_INFO_OPTION_ID_MIN > id || id > AT_COMMAND_INFO_OPTION_ID_MAX) {
+		return 0;
+	}
 
-	// write option to buffer
-	snprintf(buffer + cursor, AT_COMMAND_INFO_OPTIONS_SIZE + 1, "%d", id);
-	cursor += AT_COMMAND_INFO_OPTIONS_SIZE;
+	// write command and option to buffer
+	snprintf(buffer, size + 1, AT_COMMAND_INFO, id);
 
 	// write buffer to stream
 	ATDevice_write(self, buffer, 0, size);
 }
 
 int ATDevice_setLoudness(struct ATDevice * self, AT_COMMAND_LOUDNESS_OPTION level) {
-	size_t cursor = 0;
 	size_t size = AT_COMMAND_LOUDNESS_SIZE + AT_COMMAND_LOUDNESS_OPTIONS_SIZE;
 	char buffer[size];
 
-	// ensure level is between 0 and 3
-	if (0 < level || level > 3) {
-		return 0;
-	}
-
-	// write command to buffer
-	strncpy(buffer + cursor, AT_COMMAND_LOUDNESS, AT_COMMAND_LOUDNESS_SIZE);
-	cursor += AT_COMMAND_LOUDNESS_SIZE;
-
-	// write option to buffer
-	snprintf(buffer + cursor, AT_COMMAND_LOUDNESS_OPTIONS_SIZE, "%d", level);
+	// write command and option to buffer
+	snprintf(buffer, size + 1, AT_COMMAND_LOUDNESS, level);
 
 	// write buffer to stream
 	ATDevice_write(self, buffer, 0, size);
 }
 
 int ATDevice_setMute(ATDevice * self, AT_COMMAND_MUTE_OPTION option) {
-	size_t cursor = 0;
 	size_t size = AT_COMMAND_MUTE_SIZE + AT_COMMAND_MUTE_OPTIONS_SIZE;
 	char buffer[size];
 
-	strncpy(buffer + cursor, AT_COMMAND_MUTE, AT_COMMAND_MUTE_SIZE);
-	cursor += AT_COMMAND_MUTE_SIZE;
+	// write command and option to buffer
+	snprintf(buffer, size + 1, AT_COMMAND_MUTE, (int)option);
 
-	snprintf(buffer + cursor, AT_COMMAND_MUTE_OPTIONS_SIZE, "%d", option);
-	cursor += AT_COMMAND_MUTE_OPTIONS_SIZE;
-
+	// write buffer to stream
 	return ATDevice_write(self, buffer, 0, size);
 }
 
@@ -198,7 +175,7 @@ int ATDevice_getRegister(ATDevice * self, unsigned int n, char ** value, size_t 
 	size_t size = AT_COMMAND_REGISTER_GET_SIZE + AT_COMMAND_REGISTER_OPTION_N_SIZE;
 	char buffer[size];
 
-	snprintf(buffer, size, AT_COMMAND_REGISTER_GET, n);
+	snprintf(buffer, size + 1, AT_COMMAND_REGISTER_GET, n);
 
 	return ATDevice_write(self, buffer, 0, size);
 }
